8ce0b5971f86a8f0aad2792c2c5443af243e1347
thalesrmacedo <165821269+thalesrmacedo@users.noreply.github.com>
2025-07-28 15:49:15 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
-import os
+
+load_dotenv()
 
 def create_app():
-    load_dotenv()
-    app = Flask(__name__)
-    app.config['STRIPE_SECRET_KEY'] = os.getenv("STRIPE_SECRET_KEY")
-    app.config['STRIPE_PUBLIC_KEY'] = os.getenv("STRIPE_PUBLIC_KEY")
+    template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+    app = Flask(__name__, template_folder=template_dir)
+    app.config['STRIPE_SECRET_KEY'] = os.getenv('STRIPE_SECRET_KEY')
+    app.config['STRIPE_PUBLIC_KEY'] = os.getenv('STRIPE_PUBLIC_KEY')
 
     from .routes import main
     app.register_blueprint(main)
